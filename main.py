0eb8d831392609ce50bc97dc5f106009f97bbaaf
XanderWatson <saahil_bhavsar@outlook.com>
2023-12-15 19:48:34 +0530

    Adds error handling

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 
 
-def chat(model="gemini-pro"):
-    model = genai.GenerativeModel(model)
+def chat(model_name="gemini-pro", generation_config={}, safety_settings=[]):
+    model = genai.GenerativeModel(
+        model_name=model_name,
+        generation_config=generation_config,
+        safety_settings=safety_settings
+    )
+
     chat = model.start_chat(history=[])
 
     while True:
@@ -15,16 +20,23 @@ def chat(model="gemini-pro"):
             print("\nBye :)")
 
             break
-
-        response = chat.send_message(user_msg, stream=True)
         
-        print("\nAssistant:", end=" ")
+        try:
+            response = chat.send_message(user_msg, stream=True)
 
-        for chunk in response:
-            print(f"{chunk.text}", end="")
+            print("\nAssistant:", end=" ")
 
-        print("\n")
+            for chunk in response:
+                print(f"{chunk.text}", end="")
 
+            print("\n")
+        except Exception as e:
+            print(f"\n{e}\n")
+
+            try:
+                chat.rewind()
+            except Exception:
+                pass
 
 if __name__ == "__main__":
     load_dotenv()
@@ -32,5 +44,28 @@ if __name__ == "__main__":
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     genai.configure(api_key=GOOGLE_API_KEY)
     
-    chat()
+    generation_config = {
+        "temperature": 0.9,
+    }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_ONLY_HIGH"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_ONLY_HIGH"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_ONLY_HIGH"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_ONLY_HIGH"
+        }
+    ]
+
+    chat(generation_config=generation_config, safety_settings=safety_settings)
 
